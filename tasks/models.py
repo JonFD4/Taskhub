896d0d82e38c45dab7946dde4910b010cc120a79
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-05-19 20:32:27 +0000

    import resizedimage

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User as DjangoUser
-from resized import ResizedImageField
+from django_resized import ResizedImageField
+
 
 
 class Category(models.Model):
