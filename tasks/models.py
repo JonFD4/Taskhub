b548282a3889783bf6cb3a30e1d62f141e150344
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-05-20 20:19:34 +0000

    Import cloudinary storage to ensure goal-image is stored in cloudniary

--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User as DjangoUser
 from django_resized import ResizedImageField
-
+from cloudinary_storage.storage import MediaCloudinaryStorage
 
 
 class Category(models.Model):
@@ -26,7 +26,7 @@ class Task(models.Model):
         ('L', 'Low'),
     ]
  
-    user = models.ForeignKey(DjangoUser, on_delete=models.CASCADE, help_text='The user who created the task')
+    user = models.ForeignKey(DjangoUser, on_delete=models.CASCADE, null=True, blank=True, help_text='The user who created the task')
     title = models.CharField(max_length=200, help_text='Enter the title of the task')
     due_date = models.DateField(help_text='Enter the due date for the task')
     due_time = models.TimeField(null=True, blank=True, help_text='Enter the due time for the task (optional)')
@@ -34,8 +34,8 @@ class Task(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, help_text='The category to which the task belongs')
     priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, help_text='The priority level of the task')
     completed = models.BooleanField(default=False, help_text='Indicates if the task is completed')
-    goal_image = ResizedImageField(size=[400, 300], quality=75, upload_to='goal_images/', null=True, blank=True, help_text='Upload a resized image related to the goal (optional)')
-    goal_image_alt = models.Charfield(max_length=100, null=False, blank=True, help_text='Describe your image')
+    goal_image = ResizedImageField(size=[400, 300], quality=75, upload_to='goal_images/', storage=MediaCloudinaryStorage(), null=True, blank=True, help_text='Upload a resized image related to the goal (optional)')
+    goal_image_alt = models.CharField(max_length=100, null=False, blank=True, help_text='Describe your image')
 
     def __str__(self):
         return str(self.title)
