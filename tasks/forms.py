f7c0b288da0b1e4a361ba7d888242f19aa9d3a7e
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-05-22 18:31:41 +0000

    Set fields to show when editing task

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,7 +5,7 @@ from django_summernote.widgets import SummernoteWidget
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = '__all__'
+        fields = ['title', 'due_date', 'due_time', 'category', 'priority', 'completed', 'additional_info', 'goal_image', 'goal_image_alt']
         widgets = {
             'additional_info': SummernoteWidget(),
         }
