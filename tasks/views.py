b43641990b3369c5a7bcacf3808c9886c360c802
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-05-22 18:25:08 +0000

    Funcrions for update and delete task

--- tasks/views.py
+++ tasks/views.py
@@ -27,6 +27,33 @@ def read_task(request, pk):
     return render(request, 'tasks/task_detail.html', {'task': task})
 
 @login_required
+def update_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if task.user != request.user:
+        return HttpResponseForbidden()
+    
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_detail', pk=task.pk)
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'tasks/update_task.html', {'form': form, 'task': task})
+
+@login_required
+def delete_task(request, pk):
+    # Handle deletion of an existing task
+    task = get_object_or_404(Task, pk=pk)
+    if task.user != request.user:
+        return HttpResponseForbidden()
+    
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'tasks/delete_task_confirmation.html', {'task': task})
+
+@login_required
 def task_list(request):
     # Retrieve all tasks for the logged-in user. Dashboard
     tasks = Task.objects.filter(user=request.user)
