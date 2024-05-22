76443127457d93ff9bd61d812704abb7bc496716
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-05-22 18:20:49 +0000

    Create function for create_task

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+@login_required
+def create_task(request):
+    # Handle creation of a new task
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/create_task.html', {'form': form})
