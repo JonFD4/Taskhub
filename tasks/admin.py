ab4bd39bc68ed6023f61c1c95ed1df9d1e480de1
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-05-22 18:29:38 +0000

    functions to make user field read only and set user field

--- tasks/admin.py
+++ tasks/admin.py
@@ -10,20 +10,40 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    form = TaskForm
-    list_display = ('title', 'due_date', 'due_time', 'user', 'category', 'priority', 'completed')
+    list_display = ('title', 'due_date', 'due_time', 'user', 'category', 'priority', 'completed', 'goal_image')
     list_filter = ('due_date', 'user', 'category', 'priority', 'completed')
     search_fields = ('title', 'additional_info')
     list_editable = ('completed',)
     list_display_links = ('title',)
     date_hierarchy = 'due_date'
     ordering = ('-due_date', 'priority')
-    readonly_fields = ('user',)
     fieldsets = (
         (None, {
-            'fields': ('title', 'due_date', 'due_time', 'additional_info')
+            'fields': ('title', 'due_date', 'due_time', 'additional_info', 'goal_image', 'goal_image_alt')
         }),
         ('Task Details', {
             'fields': ('user', 'category', 'priority', 'completed'),
         }),
     )
+
+   # This method specifies which fields should be read-only in the admin interface.
+# When editing an existing object, the 'user' field is made read-only,
+# ensuring it cannot be modified once set.
+# When creating a new object, all fields remain editable.
+def get_readonly_fields(self, request, obj=None):
+    if obj: 
+        return ['user']
+    return []
+
+# This method overrides the default save behavior for the model in the admin interface.
+# It ensures that if the 'user' field is not set when saving the object,
+# it is automatically assigned the current user.
+def save_model(self, request, obj, form, change):
+    if not obj.user:
+        obj.user = request.user
+    obj.save()
+
+
+
+
+
