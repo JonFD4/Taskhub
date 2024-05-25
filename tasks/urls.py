033a414e17b93cd9a4935cb297d58aabaa7aa1b0
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-05-25 18:14:25 +0000

    Create category urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('task/create/', views.create_task, name='create_task'),
     path('task/update/<int:pk>/', views.update_task, name='update_task'),
     path('task/delete/<int:pk>/', views.delete_task, name='delete_task'),
+
+    # Category urls
+path('category/', views.category_list, name='category_list'),
+    path('create/', views.create_category, name='create_category'),
+    path('<int:category_id>/update/', views.update_category, name='update_category'),
+    path('category/<int:category_id>/delete/', views.delete_category, name='delete_category'),
 ]
