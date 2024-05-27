2b8b46f479736eb7d26ba5c2ffd389d570d02aff
Jonathan Forson <jonathanforsondanso@gmail.com>
2024-05-27 18:45:19 +0000

    Align paths evenly

--- tasks/urls.py
+++ tasks/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('task/delete/<int:pk>/', views.delete_task, name='delete_task'),
 
     # Category urls
-path('category/', views.category_list, name='category_list'),
+    path('category/', views.category_list, name='category_list'),
     path('create/', views.create_category, name='create_category'),
     path('<int:category_id>/update/', views.update_category, name='update_category'),
     path('category/<int:category_id>/delete/', views.delete_category, name='delete_category'),
